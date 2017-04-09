8e1b62132ba4cde57185c246299a637b5ec20c48
Connor Montgomery <connor@pinterest.com>
2017-04-08 19:51:20 -0700

    Initial Commit

---
+++ WUNDER/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WUNDER
+//
+//  Created by Connor Montgomery on 4/8/17.
+//  Copyright © 2017 Connor Montgomery. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WUNDER/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WUNDER
+//
+//  Created by Connor Montgomery on 4/8/17.
+//  Copyright © 2017 Connor Montgomery. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
